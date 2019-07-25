1df9ef7e55c0e005e52975e6d9fd4e227d58e68e
Andrei Popa <andrei@hashicorp.com>
2019-07-25 14:15:27 +0200

    add remote

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "popa-org"
+
+    workspaces {
+      name = "my-app-dev"
+    }
+  }
+}
+
 resource "null_resource" "infra" {
     provisioner "local-exec" {
         command = "echo null infra"
