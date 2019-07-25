5537ebd6f15a1d474228edc9546f0b7cab41ed17
Andrei Popa <andrei@hashicorp.com>
2019-07-25 14:40:11 +0200

    add new staff

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ resource "null_resource" "infra" {
         command = "echo null infra"
     }
 }
+
+resource "random_id" "server" {
+  byte_length = 8
+}
+
+output "random_server" {
+  value = "${random_id.server.id}"
+  description = "Random resource ID"
+}
